c1fc902fdeb24ea7b137ec97e1428cb22d307cf0
Siddhi <siddhiup06@gamil.com>
2025-06-18 23:16:22 +0530

    Remove Project/GREENCART directory

--- Project/GREENCART/server/routes/cartRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import authUser from "../middlewares/authUser.js";
-import { updateCart } from "../controllers/cartController.js";
-
-const cartRouter = express.Router();
-
-cartRouter.post('/update',authUser,updateCart)
-
-export default cartRouter;
